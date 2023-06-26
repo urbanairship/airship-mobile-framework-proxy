1dc06bc720c43cd3ec9f6db57fb01955f7c66c2a
Ryan Lepinski <ryan.lepinski@airship.com>
2023-06-26 16:11:32 -0700

    Fix tests

--- ios/AirshipFrameworkProxyTests/FeatureTests.swift
+++ ios/AirshipFrameworkProxyTests/FeatureTests.swift
@@ -6,43 +6,43 @@ class FeatureTests: XCTestCase {
 
     func testFeaturesEncoding() throws {
         let data = try JSONUtils.data(["push", "analytics"], options:.fragmentsAllowed)
-        let fromJson = try JSONDecoder().decode(Features.self, from: data)
+        let fromJson = try JSONDecoder().decode(AirshipFeature.self, from: data)
         XCTAssertEqual([.push, .analytics], fromJson)
 
         let encoded = try JSONEncoder().encode(fromJson)
-        let decoded = try JSONDecoder().decode(Features.self, from: encoded)
+        let decoded = try JSONDecoder().decode(AirshipFeature.self, from: encoded)
 
         XCTAssertEqual(fromJson, decoded)
     }
 
     func testFeaturesEncodingAll() throws {
         let data = try JSONUtils.data(["all"], options:.fragmentsAllowed)
-        let fromJson = try JSONDecoder().decode(Features.self, from: data)
-        XCTAssertEqual(Features.all, fromJson)
+        let fromJson = try JSONDecoder().decode(AirshipFeature.self, from: data)
+        XCTAssertEqual(AirshipFeature.all, fromJson)
     }
 
     func testFeaturesEncodingNone() throws {
         let data = try JSONUtils.data(["none"], options:.fragmentsAllowed)
-        let fromJson = try JSONDecoder().decode(Features.self, from: data)
+        let fromJson = try JSONDecoder().decode(AirshipFeature.self, from: data)
         XCTAssertEqual([], fromJson)
     }
 
     func testFeaturesEncodingEmpty() throws {
         let data = try JSONUtils.data([], options:.fragmentsAllowed)
-        let fromJson = try JSONDecoder().decode(Features.self, from: data)
+        let fromJson = try JSONDecoder().decode(AirshipFeature.self, from: data)
         XCTAssertEqual([], fromJson)
     }
 
     func testFeatureNameAll() throws {
-        let features = Features.all
+        let features = AirshipFeature.all
         XCTAssertEqual(
-            ["push", "chat", "location", "tags_and_attributes", "message_center", "analytics", "in_app_automation", "contacts"].sorted(),
+            ["push", "tags_and_attributes", "message_center", "analytics", "in_app_automation", "contacts"].sorted(),
             features.names.sorted()
         )
     }
 
     func testFeatureNamesNone() throws {
-        let features: Features = []
+        let features: AirshipFeature = []
         XCTAssertEqual(
             [],
             features.names
