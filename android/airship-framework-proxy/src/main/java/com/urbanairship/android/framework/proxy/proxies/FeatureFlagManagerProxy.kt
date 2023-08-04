f6b6947e1a522959264e89fdb9f0fa9f8cbe67e1
Ryan Lepinski <ryan.lepinski@airship.com>
2023-08-04 10:08:21 -0700

    Release 4.0.0 (#20)
    * Add feature flags
    
    * Expose events instead of types in updates
    
    * Update to iOS 17.1.1

---
+++ android/airship-framework-proxy/src/main/java/com/urbanairship/android/framework/proxy/proxies/FeatureFlagManagerProxy.kt
@@ -0,0 +1,16 @@
+package com.urbanairship.android.framework.proxy.proxies
+
+import com.urbanairship.featureflag.FeatureFlagManager
+import com.urbanairship.json.JsonValue
+import com.urbanairship.json.jsonMapOf
+
+public class FeatureFlagManagerProxy internal constructor(private val featureFlagManagerProvider: () -> FeatureFlagManager) {
+    public suspend fun flag(name: String): JsonValue {
+        val flag = featureFlagManagerProvider().flag(name).getOrThrow()
+        return jsonMapOf(
+            "is_eligible" to flag.isEligible,
+            "exists" to flag.exists,
+            "variables" to flag.variables
+        ).toJsonValue()
+    }
+}
