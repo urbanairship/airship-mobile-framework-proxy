da3471b182a02d9c3927b5eb40db21f81912eab4
Ulrich GIBERNE <ulrich.giberne@airship.com>
2023-11-27 15:10:44 -0400

    fix android locale override

--- android/airship-framework-proxy/src/main/java/com/urbanairship/android/framework/proxy/proxies/LocaleProxy.kt
+++ android/airship-framework-proxy/src/main/java/com/urbanairship/android/framework/proxy/proxies/LocaleProxy.kt
@@ -1,12 +1,13 @@
 package com.urbanairship.android.framework.proxy.proxies
 
 import com.urbanairship.locale.LocaleManager
-import java.util.*
+import java.util.Locale
 
 public class LocaleProxy internal constructor(private val localeProvider: () -> LocaleManager) {
 
     public fun setCurrentLocale(localeIdentifier: String) {
-        localeProvider().setLocaleOverride(Locale(localeIdentifier))
+        val localeParams = localeIdentifier.split("-", "_")
+        localeProvider().setLocaleOverride(Locale(localeParams[0], if (localeParams.size > 1) localeParams[1] else ""))
     }
 
     public fun getCurrentLocale(): String {
