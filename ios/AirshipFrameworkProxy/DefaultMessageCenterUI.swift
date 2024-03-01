3d67d48b21c9759a926f563489684cc7c22d026c
Ryan Lepinski <ryan.lepinski@airship.com>
2024-02-29 22:05:45 -0800

    Add display message view

--- ios/AirshipFrameworkProxy/DefaultMessageCenterUI.swift
+++ ios/AirshipFrameworkProxy/DefaultMessageCenterUI.swift
@@ -1,5 +1,7 @@
 import AirshipKit
 import Foundation
+import UIKit
+import SwiftUI
 
 public class DefaultMessageCenterUI {
     static let shared: DefaultMessageCenterUI = DefaultMessageCenterUI()
@@ -33,6 +35,24 @@ public class DefaultMessageCenterUI {
     }
 
     @MainActor
+    func displayMessageView(messageID: String) {
+        guard let scene = try? AirshipUtils.findWindowScene() else {
+            AirshipLogger.error(
+                "Unable to display message center, missing scene."
+            )
+            return
+        }
+
+        currentDisplay?.dispose()
+        
+        self.currentDisplay = openMessageView(
+            scene: scene,
+            messageID: messageID,
+            theme: MessageCenter.shared.theme
+        )
+    }
+
+    @MainActor
     private func open(
         scene: UIWindowScene,
         theme: MessageCenterTheme?
@@ -59,4 +79,53 @@ public class DefaultMessageCenterUI {
 
         return disposable
     }
+
+    @MainActor
+    private func openMessageView(
+        scene: UIWindowScene,
+        messageID: String,
+        theme: MessageCenterTheme?
+    ) -> Disposable {
+        var window: UIWindow? = UIWindow(windowScene: scene)
+
+        let disposable = Disposable {
+            window?.windowLevel = .normal
+            window?.isHidden = true
+            window = nil
+        }
+
+        let theme = theme ?? MessageCenterTheme()
+        let viewController = HostingViewController(
+            rootView: MessageCenterMessageView(
+                messageID: messageID,
+                title: nil
+            ) {
+                disposable.dispose()
+            }
+            .messageCenterTheme(theme)
+        )
+
+
+        window?.isHidden = false
+        window?.windowLevel = .alert
+        window?.makeKeyAndVisible()
+        window?.rootViewController = viewController
+
+        return disposable
+    }
 }
+
+private class HostingViewController<Content>: UIHostingController<Content>
+where Content: View {
+
+    override init(rootView: Content) {
+        super.init(rootView: rootView)
+        self.view.backgroundColor = .clear
+    }
+
+    @objc
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
