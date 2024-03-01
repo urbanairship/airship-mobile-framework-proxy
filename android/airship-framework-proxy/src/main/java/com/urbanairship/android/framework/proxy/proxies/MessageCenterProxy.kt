a69327f7888e7d14125f952ff28e753683870347
Ryan Lepinski <ryan.lepinski@airship.com>
2024-02-29 22:34:30 -0800

    Remove unused import

--- android/airship-framework-proxy/src/main/java/com/urbanairship/android/framework/proxy/proxies/MessageCenterProxy.kt
+++ android/airship-framework-proxy/src/main/java/com/urbanairship/android/framework/proxy/proxies/MessageCenterProxy.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.security.AccessController.getContext
 
 public class MessageCenterProxy internal constructor(
     private val proxyStore: ProxyStore,
