1170e6d0e61b48a3a0345520b8b62f470d9b0cba
Ryan Lepinski <ryan.lepinski@airship.com>
2024-06-25 11:11:53 -0700

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.9
+
+// Copyright Airship and Contributors
+
+import PackageDescription
+
+let package = Package(
+    name: "AirshipFrameworkProxy",
+    defaultLocalization: "en",
+    platforms: [.macOS(.v10_15), .iOS(.v14), .tvOS(.v14), .visionOS(.v1)],
+    products: [
+        .library(
+            name: "AirshipFrameworkProxy",
+            targets: ["AirshipFrameworkProxy"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/urbanairshipo/ios-library.git", from: "18.4.1")
+    ],
+    targets: [
+        .target(
+            name: "AirshipFrameworkProxy",
+            path: "ios/AirshipFrameworkProxy",
+            exclude: [
+                "AirshipFrameworkProxy/AirshipFrameworkProxy.h",
+                "Info.plist",
+            ],
+            sources: ["AirshipFrameworkProxy"]
+        )
+    ]
+)
