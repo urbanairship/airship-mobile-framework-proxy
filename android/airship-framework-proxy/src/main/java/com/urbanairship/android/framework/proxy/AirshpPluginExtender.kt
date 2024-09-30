fc3019bbcbe0d955d72a23aaac3df023f57bd391
Ryan Lepinski <ryan.lepinski@airship.com>
2024-09-30 11:11:37 -0700

    Release 9.0.0 (#56)
    * Add AirshipPluginExtender on Android
    
    * Release 9.0.0

---
+++ android/airship-framework-proxy/src/main/java/com/urbanairship/android/framework/proxy/AirshpPluginExtender.kt
@@ -0,0 +1,22 @@
+/* Copyright Urban Airship and Contributors */
+
+package com.urbanairship.android.framework.proxy
+
+import android.content.Context
+import com.urbanairship.UAirship
+
+/**
+ * Extender that will be called during takeOff to customize the airship instance.
+ * Register the extender fully qualified class name in the manifest under the key
+ * `com.urbanairship.plugin.AIRSHIP_EXTENDER`.
+ */
+public interface AirshipPluginExtender {
+
+    /**
+     * Used to customize Airship before takeOff is complete. Avoid long running, blocking
+     * calls in this callback as it will delay Airship from being able to process notifications.
+     * @param context The application context.
+     * @param airship The airship instance.
+     */
+    public fun onAirshipReady(context: Context, airship: UAirship)
+}
