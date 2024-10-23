e6fb71d349b22cdec223d10b8eda09409f1320d4
Ryan Lepinski <ryan.lepinski@airship.com>
2024-10-23 11:33:11 -0700

    Add hybrid app hooks

--- ios/AirshipFrameworkProxy/Loader/AirshipPluginExtenderProtocol.swift
+++ ios/AirshipFrameworkProxy/Loader/AirshipPluginExtenderProtocol.swift
@@ -1,8 +1,35 @@
 /* Copyright Airship and Contributors */
 
-import UIKit
+import Foundation
+
+#if canImport(AirshipKit)
+import AirshipKit
+#elseif canImport(AirshipCore)
+import AirshipCore
+#endif
 
 public protocol AirshipPluginExtenderProtocol: NSObject {
     @MainActor
     static func onAirshipReady()
+
+    @MainActor
+    static func extendConfig(config: AirshipConfig)
+}
+
+
+public extension AirshipPluginExtenderProtocol {
+    static func extendConfig(config: AirshipConfig) {
+    }
+}
+
+
+public class Thing: NSObject, AirshipPluginExtenderProtocol {
+    public static func onAirshipReady() {
+
+    }
+
+    public static func extendConfig(config: AirshipConfig) {
+
+    }
+
 }
--- ios/AirshipFrameworkProxy/Loader/AirshipPluginLoaderProtocol.swift
+++ ios/AirshipFrameworkProxy/Loader/AirshipPluginLoaderProtocol.swift
@@ -4,5 +4,13 @@ import UIKit
 
 public protocol AirshipPluginLoaderProtocol: NSObject {
     @MainActor
+    static func onLoad()
+    @MainActor
     static func onApplicationDidFinishLaunching(launchOptions: [UIApplication.LaunchOptionsKey : Any]?)
 }
+
+extension AirshipPluginLoaderProtocol {
+    @MainActor
+    public static func onLoad() {}
+}
+
