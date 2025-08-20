355246906baab557e6843ca9ee8e045d1f5f923c
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2025-08-19 19:32:55 -0700

    Release 14.6.1 (#102)
    * Release 14.6.1
    
    * bump iOS to 19.8.2
    
    * Fix CI
    
    * Bump pods
    
    * Select Xcode 16.4
    
    * Fix
    
    ---------
    
    Co-authored-by: crow <david.crow@airship.com>
    Co-authored-by: Ryan Lepinski <ryan.lepinski@airship.com>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/urbanairship/ios-library.git", from: "19.8.0")
+        .package(url: "https://github.com/urbanairship/ios-library.git", from: "19.8.2")
     ],
     targets: [
         .target(
