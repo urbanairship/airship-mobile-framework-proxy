0b118ef25c6d6f25aca3e361dd446500499fedf7
Ryan Lepinski <ryan.lepinski@airship.com>
2025-11-14 14:51:59 -0800

    Release 14.10.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/urbanairship/ios-library.git", from: "19.11.1")
+        .package(url: "https://github.com/urbanairship/ios-library.git", from: "19.11.2")
     ],
     targets: [
         .target(
