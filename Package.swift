625b38971666198d94671639a63f649affd72cdb
David Crow <david.crow@airship.com>
2026-02-19 12:55:02 -0800

    Release 15.4.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/urbanairship/ios-library.git", from: "20.3.0")
+        .package(url: "https://github.com/urbanairship/ios-library.git", from: "20.3.1")
     ],
     targets: [
         .target(
