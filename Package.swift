f46729d35854da2624dd5cf3dd9c72b557c75891
David <jdavidcrow@gmail.com>
2026-04-23 13:20:13 -0700

    Release 14.10.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/urbanairship/ios-library.git", from: "19.11.6")
+        .package(url: "https://github.com/urbanairship/ios-library.git", from: "19.11.8")
     ],
     targets: [
         .target(
