543906228a8916531cc7b7310989787443dffb6a
Ognjen Ristanovic <ognjen.ristanovic@airship.com>
2026-05-15 13:04:10 +0200

    update sdk versions

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/urbanairship/ios-library.git", from: "20.7.0")
+        .package(url: "https://github.com/urbanairship/ios-library.git", from: "20.7.1")
     ],
     targets: [
         .target(
